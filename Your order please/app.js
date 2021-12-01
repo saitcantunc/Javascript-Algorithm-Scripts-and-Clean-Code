f298482aa04fab040a298e42092b8e65d76f566c
saitcantunc <tuncsaitcan@gmail.com>
2021-12-01 08:08:40 +0300

    1.12.2021 You order please solved!

---
+++ Your order please/app.js
@@ -0,0 +1,16 @@
+/* Your task is to sort a given string. Each word in the string will contain a single number. This number is the position the word should have in the result.
+
+Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+
+If the input string is empty, return an empty string. The words in the input String will only contain valid consecutive numbers. */
+
+function order(words){ 
+    return words.split(' ').sort(function(a,b){
+
+      return a.match(/\d/) - b.match(/\d/);
+    
+    }).join(' ');
+}
+  
+
+console.log(order("is2 Thi1s T4est 3a"))
