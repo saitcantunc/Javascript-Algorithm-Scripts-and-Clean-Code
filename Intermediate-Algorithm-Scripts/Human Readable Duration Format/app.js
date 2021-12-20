eaa3e52bd7853eb48edecf2f384ef6227b0721aa
saitcantunc <tuncsaitcan@gmail.com>
2021-12-20 08:11:49 +0300

    20.12.2021 Human Readable Duration Format Solved!

---
+++ Intermediate-Algorithm-Scripts/Human Readable Duration Format/app.js
@@ -0,0 +1,30 @@
+/* Description:
+Your task in order to complete this Kata is to write a function which formats a duration, given as a number of seconds, in a human-friendly way.
+
+The function must accept a non-negative integer. If it is zero, it just returns "now". Otherwise, the duration is expressed as a combination of years, days, hours, minutes and seconds.
+
+It is much easier to understand with an example:
+
+formatDuration(62)    // returns "1 minute and 2 seconds"
+formatDuration(3662)  // returns "1 hour, 1 minute and 2 seconds" */
+
+function formatDuration(seconds) {
+  var time = { year: 31536000, day: 86400, hour: 3600, minute: 60, second: 1 },
+    res = [];
+
+  if (seconds === 0) return "now";
+
+  for (var key in time) {
+    if (seconds >= time[key]) {
+      var val = Math.floor(seconds / time[key]);
+      res.push((val += val > 1 ? " " + key + "s" : " " + key));
+      seconds = seconds % time[key];
+    }
+  }
+
+  return res.length > 1
+    ? res.join(", ").replace(/,([^,]*)$/, " and" + "$1")
+    : res[0];
+}
+
+console.log(formatDuration(3454352543));
