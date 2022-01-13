d4037bf0c9881f02eee7d391dbec57c3958659f1
saitcantunc <tuncsaitcan@gmail.com>
2022-01-13 06:36:57 +0300

    Duplicate Encoder Added Best Practice

--- Basic-Algorithm-Scripts/Duplicate Encoder/app.js
+++ Basic-Algorithm-Scripts/Duplicate Encoder/app.js
@@ -3,18 +3,31 @@ if that character appears only once in the original string, or ")" if that chara
 the original string. 
 Ignore capitalization when determining if a character is a duplicate. */
 
-function duplicateEncode(word){
-    var letterCount = {};
-    var letters = word.toLowerCase().split('');
-  
-    letters.forEach(function(letter) {
-      letterCount[letter] = (letterCount[letter] || 0) + 1;
-    });
-  
-    return letters.map(function(letter) {
-      return letterCount[letter] === 1 ? '(' : ')';
-    }).join('');
+function duplicateEncode(word) {
+	var letterCount = {};
+	var letters = word.toLowerCase().split("");
+
+	letters.forEach(function (letter) {
+		letterCount[letter] = (letterCount[letter] || 0) + 1;
+	});
+
+	return letters
+		.map(function (letter) {
+			return letterCount[letter] === 1 ? "(" : ")";
+		})
+		.join("");
 }
-  
-  
-console.log(duplicateEncode("re cede"))
+
+console.log(duplicateEncode("re cede"));
+
+/* Best Practice */
+
+function duplicateEncode2(word) {
+	return word
+		.toLowerCase()
+		.split("")
+		.map(function (a, i, w) {
+			return w.indexOf(a) == w.lastIndexOf(a) ? "(" : ")";
+		})
+		.join("");
+}
